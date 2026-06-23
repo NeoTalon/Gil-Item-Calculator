059941b4a4b97275322d26073dc00d5bf5fa3aef
NeoTalon <szarazdomi@gmail.com>
2026-06-23 11:53:17 +0200

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
 
 public class Class1
 {
 	public Class1()
 	{
+		Console.Write();
 	}
 }
